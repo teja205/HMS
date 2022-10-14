fe8bdeb90a1c991f73fd08a88a0a05b5d99ecf8e
navsnavya <91525143+Bhanumanjari@users.noreply.github.com>
2022-10-14 19:33:10 +0530

    add sidebar

--- src/Constant/images.js
+++ src/Constant/images.js
@@ -1,15 +1,14 @@
 import logo from '../Assets/images/logo.png';
 import bg from '../Assets/images/background.png';
 import loginLogo from '../Assets/images/loginLogo.png';
-import uploadIcon from '../Assets/images/uploadicon.png'
-
-
-
+import hmslogo from '../Assets/images/hms-logo.png';
+import profilepic from '../Assets/images/face1.jpg';
 
 
 export const assetsImages = {
     logo : logo,
     bg : bg,
     loginLogo  : loginLogo ,
-    uploadIcon : uploadIcon,
+    hmslogo : hmslogo,
+    profilepic : profilepic,
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react'
 import Table from '../Component/table'
 import Header from './header'
+//import Sidebar from './Sidebar';
 import { assetsImages } from '../Constant/images';
 
 export class Home extends Component {
   render() {
-    return (
-      <div className='home'>
-          <img
-                  className="background"
-                  src={assetsImages.bg}
-                  alt="Catersmart Logo"
-                /> 
-          <Header />
+    return (      
+      <div className='home row'>
+        <div className="col-12">
+        <Header />
+        </div>
+        <div className="col-3">
+          Hello
+        </div>
+        <div className="col-9">          
           <Table />
+        </div>
       </div>
     )
   }
